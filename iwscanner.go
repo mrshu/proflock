f41c6e1224554cb55431807f1bf4bdebf342ae6c
mr.Shu <mr@shu.io>
2014-02-20 12:44:38 +0100

    iwscanner redone

--- iwscanner.go
+++ iwscanner.go
@@ -5,6 +5,23 @@ import (
         "strings"
 )
 
+func IsWifiOn() bool {
+        var out bytes.Buffer
+        testcmd := exec.Command("iwconfig", "wlan0")
+        testcmd.Stdout = &out
+        if (e := testcmd.Run(), e != nil) {
+                return false
+        }
+
+        contains_up := strings.Contains(out.String(), "UP")
+
+        if (contains_up && in == "on") {
+                return true
+        } else {
+                return false
+        }
+}
+
 // in can have strings "on" or "off"
 func TurnWifi(in string) bool {
         var t string
@@ -19,19 +36,13 @@ func TurnWifi(in string) bool {
         if err := cmd.Run(), err != nil {
                 return false
         } else {
-                var out bytes.Buffer
-                testcmd := exec.Command("iwconfig", "wlan0")
-                testcmd.Stdout = &out
-                if (e := testcmd.Run(), e != nil) {
-                        return false
-                }
-
-                contains_up := strings.Contains(out.String(), "UP")
-
-                if ((contains_up && in == "on") || (!contains_up && in != "on")) {
+                if in == "on" && IsWifiOn() {
                         return true
-                } else {
-                        return false
                 }
+                if in == "off" && !IsWifiOn() {
+                        return true
+                }
+
+                return false
         }
 }
